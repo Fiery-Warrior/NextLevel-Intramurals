6399fdaa3390d8267152b7917456ea336c51ec1a
Keagan Bogart <keagan.j.bogart@gmail.com>
2023-09-22 21:33:01 -0500

    Inital Next Level Page

---
+++ nextlevel/src/HomePage/homepage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+        <p>
+          next level community intramurals 
+        </p>
+    </div>
+  );
+}
+
+export default HomePage;

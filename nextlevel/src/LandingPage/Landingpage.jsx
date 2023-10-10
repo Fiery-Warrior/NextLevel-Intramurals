e881a4fe6da2ad4238801608fe8e8c0453cb6f61
SecureB00t <ashercoates@yahoo.com>
2023-10-10 13:59:38 -0500

    Testcommit

--- nextlevel/src/LandingPage/Landingpage.jsx
+++ nextlevel/src/LandingPage/Landingpage.jsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import './NLCI.css';
-// import { Button } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/styles.css';
-// function LandingPage() {
-//   return (
-//     <div className="landing-page">
-//       <div className="landing-page-text">
-//         <h1>Next Level</h1>
-//         <h2>Community</h2>
-//         <h2>Intramurals</h2>
-
-//         <p> Since 2023 </p>
-
-//         <Button variation="primary">Join Now</Button>
-
-//       </div>
-
-//       <img src={process.env.PUBLIC_URL + '/trophy.png'} alt="Trophy" />
-
-
-
-//     </div>
-
-//   );
-// }
-// export default LandingPage;
-
-
 import React from 'react';
 import './landingImage.css';
 import './NLCI.css';
@@ -44,6 +15,7 @@ function LandingPage() {
         <p> Since I don't remember when </p>
 
         <Button variation="primary">Join Now</Button>
+        <Button style={{ marginTop: '10px' }}>Login</Button>
 
       </div>
       <div className="landing-page-image">

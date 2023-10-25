a4a6b92841d7f53a65b2399bd722b80a01a7400f
Fiery-Warrior <keagan.j.bogart@gmail.com>
2023-10-25 17:42:55 -0500

    updates

--- nextlevel/index.js
+++ nextlevel/index.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'NLIDB'
+  database: 'sys'
 });
 
 connection.connect((err) => {

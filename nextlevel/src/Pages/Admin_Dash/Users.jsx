d84663dcd723def79a38225f59f42a19695cdc47
Fiery-Warrior <keagan.j.bogart@gmail.com>
2023-11-05 11:01:22 -0600

    the user data from DB is shown on the Admin page

--- nextlevel/src/Pages/Admin_Dash/Users.jsx
+++ nextlevel/src/Pages/Admin_Dash/Users.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,90 +7,65 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import axios from 'axios';
 
 // Generate Order Data
 function createData(id, date, name, sport, email, phone) {
     return { id, date, name, sport, email, phone };
 }
 
-const rows = [
-    createData(
-        0,
-        '16 Mar, 2019',
-        'John Doe',
-        'Hockey',
-        'johndoe@example.com',
-        '555-555-5555',
-    ),
-    createData(
-        1,
-        '16 Mar, 2019',
-        'Jane Doe',
-        'Quidditch',
-        'janedoe@example.com',
-        '555-555-5555',
-    ),
-    createData(
-        2,
-        '16 Mar, 2019',
-        'Bob Smith',
-        'Hockey',
-        'bobsmith@example.com',
-        '555-555-5555',
-    ),
-    createData(
-        3,
-        '16 Mar, 2019',
-        'Samantha Johnson',
-        'Quidditch',
-        'samanthajohnson@example.com',
-        '555-555-5555',
-    ),
-    createData(
-        4,
-        '15 Mar, 2019',
-        'Mike Williams',
-        'Hockey',
-        'mikewilliams@example.com',
-        '555-555-5555',
-    ),
-];
-
-function preventDefault(event) {
-    event.preventDefault();
-}
-
 export default function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/admindash');
+                setUsers(response.data);
+            } catch (error) {
+                console.error(`There is error retrieving the user data: ${error}`);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    function preventDefault(event) {
+        event.preventDefault();
+    }
+
     return (
         <React.Fragment>
-            <Card>
-                <CardHeader title="User" />
-                <Table size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Sport</TableCell>
-                            <TableCell>Email</TableCell>
-                            <TableCell>Phone</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.id}>
-                                <TableCell>{row.date}</TableCell>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.sport}</TableCell>
-                                <TableCell>{row.email}</TableCell>
-                                <TableCell>{row.phone}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-                <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-                    See more Users
-                </Link>
-            </Card>
+            {users.map((user, index) => (
+                <div key={index}>
+                    <Card>
+                        <CardHeader title="User" />
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>First Name</TableCell>
+                                    <TableCell>Last Name</TableCell>
+                                    <TableCell>Email</TableCell>
+                                    <TableCell>Phone</TableCell>
+                                    <TableCell>Sport</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow key={user.id}>
+                                    <TableCell>{user.firstName}</TableCell>
+                                    <TableCell>{user.lastName}</TableCell>
+                                    <TableCell>{user.email}</TableCell>
+                                    <TableCell>{user.phone}</TableCell>
+                                    <TableCell>{user.sport}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                        <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+                            See more Users
+                        </Link>
+                    </Card>
+                </div>
+            ))}
         </React.Fragment>
     );
 }

a43b13035273d0b1d4c5ad527aad2c568d0b9aa4
Fiery-Warrior <keagan.j.bogart@gmail.com>
2023-11-16 14:02:41 -0600

    User profile showing personal logged in data including sport

--- nextlevel/src/Pages/UserProfile/UserProfile.jsx
+++ nextlevel/src/Pages/UserProfile/UserProfile.jsx
@@ -3,11 +3,20 @@ import { useCookies } from 'react-cookie';
 import CardDesign from './CardDesign';
 import { Typography } from '@material-ui/core';
 import UserNavBar from '../LandingPage/NavBar/UserNavBar';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';                
+import './userprofile.css';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 function UserProfile() {
     const [cookies] = useCookies(['myCookie']);
     const [email, setEmail] = useState('');
     const [userData, setUserData] = useState(null);
+    const [teamName, setTeamName] = useState('');
 
     useEffect(() => {
         if (cookies.myCookie && !email) {
@@ -18,7 +27,10 @@ function UserProfile() {
     useEffect(() => {
         fetch(`http://localhost:3001/userprofile/${email}`)
             .then(response => response.json())
-            .then(data => setUserData(data[0])); // assuming the response is an array
+            .then(data => {
+                setUserData(data[0]); // assuming the response is an array
+                setTeamName(data[0].TeamName); // assuming the response has a TeamName property
+            });
     }, [email]);
 
     return (
@@ -27,6 +39,7 @@ function UserProfile() {
             {/* <Typography variant="h4">{email} Profile</Typography> */}
             <br/>
             <Typography variant="h3">Welcome to your profile, {userData && userData.firstName}!</Typography>
+
 
             <h2>Feel Free to choose from some of these clubs nearby</h2>
             
@@ -41,8 +54,30 @@ function UserProfile() {
                 </div>
             )} */}
             <br/>
-            <br/>
-            <br/>
+                <h1 className="activity-cards-title">Activity Central</h1>
+
+
+                <Container maxWidth="sm" className='sport-card'>
+                    <Container maxWidth="sm" className='calender'>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DateCalendar />
+                        </LocalizationProvider>
+                    </Container>
+                    <Container maxWidth="sm">
+                            {teamName && (
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    You are part of the {teamName} team!
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    )}
+                    </Container>
+                </Container>
+                <br/>
+                <br/>
+
             <CardDesign/>
         </div>
     );

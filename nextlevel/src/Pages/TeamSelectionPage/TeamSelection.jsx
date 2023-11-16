76fbc0dbce8e63905897a8b14b841700ab18213f
McBreezy0 <jmgarr13@gmail.com>
2023-11-16 13:52:39 -0600

    adds when clicking button

--- nextlevel/src/Pages/TeamSelectionPage/TeamSelection.jsx
+++ nextlevel/src/Pages/TeamSelectionPage/TeamSelection.jsx
@@ -1,75 +1,79 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './TeamSelection.css';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  height: 80,
-  lineHeight: '80px',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-}));
+const TeamItem = ({ team, index, handleAddItemClick, addedItem }) => {
+  return (
+    <div className="search_item">
+      {team}
+      <button className="add_button" onClick={() => handleAddItemClick(index)}>
+        +
+      </button>
+    </div>
+  );
+};
 
-const lightTheme = createTheme({ palette: { mode: 'light' } });
+const Elevation = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [addedItem, setAddedItem] = useState([]); 
 
-const items = [0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => `Team ${elevation}`);
-
-export default function Elevation() {
-  const [searchQuery, setSearchQuery] = React.useState('');
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
   };
 
   const handleAddItemClick = (itemIndex) => {
-    console.log(`Add button clicked for item ${itemIndex}`);
+  
+    const newContent = `Test ${new Date().toLocaleTimeString()}`;
+    
     // Perform actions related to the item here
+
+    setAddedItem((prevContent) => [...prevContent, newContent]);
   };
+
+  const items = [0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => `Team ${elevation}`);
 
   const filteredItems = items.filter((item) =>
     item.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
-  <div>
     <div>
-      <h1 class="title">Team List</h1>
-      <input class="search_input"
-        type="text"
-        value={searchQuery}
-        onChange={handleSearch}
-        placeholder="Search for a team..."
-      />
+      <div>
+        <h1 className="title">Team List</h1>
+        <input
+          className="search_input"
+          type="text"
+          value={searchQuery}
+          onChange={handleSearch}
+          placeholder="Search for a team..."
+        />
       </div>
-      <Grid container spacing={2}>
-        {filteredItems.map((team, index) => (
-          <Grid item xs={12} key={index}>
-            <ThemeProvider theme={lightTheme}>
-              <Box
-                sx={{
-                  padding: '0 40px',
-                }}
-              >
-                <Item elevation={index}>
-                  {team}
-                  <button
-                    className="add_button"
-                    onClick={() => handleAddItemClick(index)}
-                  >
-                    +
-                  </button>
-                </Item>
-              </Box>
-            </ThemeProvider>
-          </Grid>
+      {searchQuery.length > 0 && (
+        <div className="search_list">
+          {filteredItems.map((team, index) => (
+            <TeamItem
+              key={index}
+              team={team}
+              index={index}
+              handleAddItemClick={handleAddItemClick}
+              addedItem={addedItem}
+            />
+          ))}
+          {filteredItems.length === 0 && <p>No matching teams found.</p>}
+        </div>
+      )}
+      <div className="added_items">
+        {addedItem.map((content, index) => (
+          <p key={index}>{content}</p>
         ))}
-      </Grid>
-      </div>
+    </div>
+    </div>
   );
-}
+};
+
+export default Elevation;
+
+
+
+
+
 

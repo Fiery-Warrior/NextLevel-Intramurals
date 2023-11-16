e711ec1ebc3748aaa875c4434a2943d18d95ad57
Fiery-Warrior <keagan.j.bogart@gmail.com>
2023-11-16 12:18:16 -0600

    Search feature for Admins for all userprofiles by each attribute

--- nextlevel/src/Pages/Admin_Dash/Users.jsx
+++ nextlevel/src/Pages/Admin_Dash/Users.jsx
@@ -8,13 +8,15 @@ import TableRow from '@mui/material/TableRow';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import axios from 'axios';
-
-
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Users() {
     //Use 'users' to display this data on website
     //Use 'setUsers' to grab this data
     const [users, setUsers] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -33,10 +35,53 @@ export default function Users() {
         event.preventDefault();
     }
 
+    const filteredUsers = users.filter((user) => {
+        const searchRegex = new RegExp(searchTerm, 'i');
+        return (
+            searchRegex.test(user.firstName) ||
+            searchRegex.test(user.lastName) ||
+            searchRegex.test(user.sportName) ||
+            searchRegex.test(user.stuID) ||
+            searchRegex.test(getUserPosition(user.role)) ||
+            searchRegex.test(user.TeamName)
+        );
+    });
+
+    function getUserPosition(role) {
+        switch (role) {
+            case 1:
+                return 'Player';
+            case 2:
+                return 'Captain';
+            case 3:
+                return 'Admin';
+            case 4:
+                return 'Super Admin';
+            default:
+                return 'NA';
+        }
+    }
+
     return (
         <React.Fragment>
             <Card>
                 <CardHeader title="User" />
+                <TextField
+                    label="Search"
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                    margin="normal"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
                 <Table size="small">
                     <TableHead>
                         <TableRow>
@@ -51,21 +96,7 @@ export default function Users() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {users.map((user, index) => {
-                            let userPosition;
-                            console.log(user.role)
-
-                            if (user.role === 1) {
-                                userPosition = 'Player';
-                            } else if (user.role === 2) {
-                                userPosition = 'Captain';
-                            } else if (user.role === 3) {
-                                userPosition = 'Admin';
-                            } else if (user.role === 4) {
-                                userPosition = 'Super Admin';
-                            }else {
-                                userPosition = 'NA';
-                            }
+                        {filteredUsers.map((user, index) => {
                             return (
                                 <TableRow key={index}>
                                     <TableCell>{user.firstName}</TableCell>
@@ -73,7 +104,7 @@ export default function Users() {
                                     <TableCell>{user.email}</TableCell>
                                     <TableCell>{user.sex}</TableCell>
                                     <TableCell>{user.stuID}</TableCell>
-                                    <TableCell>{userPosition}</TableCell>
+                                    <TableCell>{getUserPosition(user.role)}</TableCell>
                                     <TableCell>{user.TeamName}</TableCell>
                                     <TableCell>{user.sportName}</TableCell>
                                 </TableRow>

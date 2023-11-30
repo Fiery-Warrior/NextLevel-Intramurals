838cbf090ef8205af8de7b9023e84541892a7144
McBreezy0 <jmgarr13@gmail.com>
2023-11-29 19:09:52 -0600

    Contact Page with Form Functionality

--- nextlevel/src/Pages/ContactPage/ContactPage.jsx
+++ nextlevel/src/Pages/ContactPage/ContactPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './ContactPage.css';
 import SideImage from './Intermural1.jpeg';
+import TopNavBar from '../LandingPage/NavBar/NavBar';
+import emailjs from '@emailjs/browser';
+
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
@@ -13,37 +16,61 @@ const ContactPage = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add logic to handle form submission, e.g., send data to a server
-    console.log('Form submitted:', formData);
-    // Reset the form after submission
-    setFormData({ name: '', email: '', message: '' });
+
+    //Checks if all fields are filled out
+    if (!formData.name || !formData.email || !formData.message) {
+      alert('Please fill out all fields before submitting');
+      return;
+    }
+
+    if (isSubmitting) {
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      emailjs.sendForm('service_45r62pq', 'template_87xax4b', e.target, 'f3EnDG36a56g54ySb');
+      console.log('Email sent Successfully');
+
+      //Resets Form Data
+      setFormData({name: '', email: '', message: ''});
+    } catch (error) {
+      console.error('Error sending email', error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
-
       <div className="flex_container">
-        <img className='side_image' src={SideImage} alt="Image 1" />
+        <TopNavBar />
+        <img className='side_image' src={SideImage} alt="thing" />
+        <div className='content_container'>
       <form className='contact_form' onSubmit={handleSubmit}>
       <h2 className='contact_style'>Contact Us</h2>
-      <label>
-        Name: 
+      <label className='name_container'>
+        <h3 className='Name'>Name:</h3> 
         <input className='name_input' type="text" name="name" value={formData.name} onChange={handleChange} />
       </label>
       <br/>
-      <label>
-        Email:
+      <label className='email_container'>
+        <h3 className='Email'>Email:</h3>
         <input className='email_input' type="email" name="email" value={formData.email} onChange={handleChange} />
       </label>
       <br/>
-      <label>
-        Message:
+      <label className='message_container'>
+        <h3 className='Message'>Message:</h3>
         <textarea className='message_input' name="message" value={formData.message} onChange={handleChange} />
       </label>
-      <br />
+      <br/>
       <button className='button_input' type="submit">Submit</button>
     </form>
+    </div>
     </div>
   );
 };
